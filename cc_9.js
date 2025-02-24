f4fc4c13d515900f5dcfb4ca77107ecde1300d9b
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-02-23 19:08:55 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -19,4 +19,20 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 console.log(emp1.getDetails()); 
 console.log(emp1.calculateAnnualSalary());
 
-//Task 2
+//Task 2 Creating a Manager Class
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize; 
+    }
+    getDetails() {
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    };
+    calculateBonus() {
+        return `10% Bonus: $${this.salary * 12 * 0.10}`;
+    };
+};
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus());
